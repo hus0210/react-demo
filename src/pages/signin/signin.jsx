16cefa7fea50e9cde85289a5f5f0e373e3015b60
zouyu <zouy.fnst@cn.fujitsu.com>
2020-07-13 14:57:13 +0800

    add sign in page

--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -3,7 +3,7 @@ import LoginForm from '../../components/loginForm/loginForm.jsx'
 
 export default class SignIn extends React.Component {
     onFinish(values) {
-        console.log('Success:', this.props.match.params.class);
+        console.log('Success:',values, this.props.match.params.class);
     }
 
     onFinishFailed(errorInfo) {
