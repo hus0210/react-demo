0441252309a8d7b8ac402f38afc61ed442934c1d
zouyu <zouy.fnst@cn.fujitsu.com>
2020-07-13 16:44:43 +0800

    add calendar

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button} from 'antd';
+import { Form, Input, Button } from 'antd';
 
 const layout = {
     labelCol: { span: 8 },
@@ -12,21 +12,21 @@ const tailLayout = {
 export default class LoginForm extends React.Component {
     render() {
         return (
-            <Form name="loginForm" {...layout} initialValues={{ remember: true, }} onFinish={this.props.onFinish} onFinishFailed={this.props.onFinishFailed}>
-                <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!'}]} >
-                    <Input />
-                </Form.Item>
+                <Form name="loginForm" {...layout} initialValues={{ remember: true, }} onFinish={this.props.onFinish} onFinishFailed={this.props.onFinishFailed}>
+                    <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]} >
+                        <Input />
+                    </Form.Item>
 
-                <Form.Item label="Password" name="password" rules={[{required: true, message: 'Please input your password!'}, ]}>
-                    <Input.Password />
-                </Form.Item>
+                    <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' },]}>
+                        <Input.Password />
+                    </Form.Item>
 
-                <Form.Item {...tailLayout}>
-                    <Button type="primary" htmlType="submit">
-                        Submit
+                    <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
                     </Button>
-                </Form.Item>
-            </Form>
+                    </Form.Item>
+                </Form>
         );
     }
 };
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Empty } from 'antd';
+import { Calendar } from 'antd';
 
 export default class Home extends React.Component {
+    onPanelChange(value, mode) {
+        console.log(value.format('YYYY-MM-DD'), mode);
+    }
+
     render() {
         return (
-            <Empty />
+            <Calendar onPanelChange={this.onPanelChange} />
         );
     }
 }
