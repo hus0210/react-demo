d6058240ad698e067bace61345ec36b566d7d26a
zouyu <zouy.fnst@cn.fujitsu.com>
2020-07-13 16:07:09 +0800

    rename index to home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Empty } from 'antd';
 
-export default class Index extends React.Component {
+export default class Home extends React.Component {
     render() {
         return (
             <Empty />
--- src/pages/route.jsx
+++ src/pages/route.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Login from './login/login.jsx'
-import Index from './home/home.jsx'
+import Home from './home/home.jsx'
 import SignIn from './signin/signin.jsx'
 
 export default class Router extends React.Component {
@@ -10,7 +10,7 @@ export default class Router extends React.Component {
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/" component={Login} />
-                    <Route exact path="/index" component={Index} />
+                    <Route exact path="/home" component={Home} />
                     <Route exact path="/signin/:class" component={SignIn} />
                 </Switch>
             </BrowserRouter>
