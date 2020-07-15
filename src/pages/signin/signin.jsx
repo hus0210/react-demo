5e61fba4212f56a3e81e957901da284810c0e6e9
zouyu <zouy.fnst@cn.fujitsu.com>
2020-07-15 10:11:05 +0800

    add sign in fail and success page

--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import './signin.css'
 import LoginForm from '../../components/loginForm/loginForm.jsx'
-import { Row, Col } from 'antd';
-import { Typography } from 'antd';
+import { Row, Col, Typography, Result} from 'antd';
 
 const { Title } = Typography;
 
 export default class SignIn extends React.Component {
     onFinish(values) {
         console.log('Success:',values, this.props.match.params.class);
+        this.render = function() {
+            return (
+                <>
+                    <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
+                        <Col span={24}>
+                            < Result
+                                status="success"
+                                title="sign in success"
+                                subTitle="success"
+                            />
+                        </Col>
+                    </Row>
+                </>
+            );
+        }
+        this.forceUpdate()
     }
 
     onFinishFailed(errorInfo) {
         console.log('Failed:', errorInfo);
+        this.render = function () {
+            return (
+                <>
+                    <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
+                        <Col span={24}>
+                            < Result
+                                status="error"
+                                title="sign in error"
+                                subTitle="error"
+                            />
+                        </Col>
+                    </Row>
+                </>
+            );
+        }
+        this.forceUpdate()
     }
 
     render() {
