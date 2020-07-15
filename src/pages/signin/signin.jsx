6381d797d0447588bf5535d269d360d6cb3767d3
zouyu <zouy.fnst@cn.fujitsu.com>
2020-07-15 14:35:49 +0800

    add logo to component loginForm

--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -52,7 +52,6 @@ export default class SignIn extends React.Component {
                 <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
                     <Col xs={2} sm={4} md={6} lg={8} xl={10} ></Col>
                     <Col xs={20} sm={16} md={12} lg={8} xl={4}>
-                        <Title>FNST</Title>
                         <LoginForm onFinish={this.onFinish.bind(this)} onFinishFailed={this.onFinishFailed.bind(this)} />
                     </Col>
                     <Col xs={2} sm={4} md={6} lg={8} xl={10} ></Col>
