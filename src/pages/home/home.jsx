54962ad8e9b1008fe9c52870be9ba0168cd73b05
zouyu <zouy.fnst@cn.fujitsu.com>
2020-07-15 13:30:15 +0800

    update home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './home.css'
-import { Row, Col, Typography, Button, Table } from 'antd';
+import { Row, Col, Typography, Button, Table, Space } from 'antd';
 
 <<<<<<< HEAD
 export default class Home extends React.Component {
@@ -15,37 +15,70 @@ const { Column } = Table;
 
 export default class Home extends React.Component {
     state = {
-        dataSource: [
+        groupDataSource: [
             {
                 key: 0,
                 name: "1",
                 type: "Group",
-                children: []
             },
+        ],
+    }
+
+    expandedRowRender(record) {
+        var classDataSource = [
+            {
+                key: 0,
+                name: "11",
+                detail: "class 11"
+            },
+            {
+                key: 1,
+                name: "12",
+                detail: "class 12"
+            }
         ]
+
+        return(
+            <Table dataSource={classDataSource} pagination={false}>
+                <Column title="Name" dataIndex="name" key="name"/>
+                <Column title="Detail" dataIndex="detail" key="detail"/>
+                <Column title="Action" dataIndex="action" key="action" render={
+                    function (_, record){
+                        return(
+                            <Space size="middle">
+                                <Button type="primary" onClick={this.onEditClick.bind(this, record)}>Edit</Button>
+                                <Button type="primary" onClick={this.onDeleteClick.bind(this, record)}>Delete</Button>
+                            </Space>
+                        )
+                    }.bind(this)
+                }/>
+            </Table>
+        )
     }
 
-    onClick(value) {
+    onNewClick() {
 
     }
 
-    onExpand(expanded, record) {
-        if(expanded) {
-            var dataSource = this.state.dataSource
-            dataSource[record.key].children = [
-                {
-                    key: 0,
-                    name: "1",
-                    type: "Class",
-                }
-            ]
-            this.setState({
-                dataSource: dataSource
-            })
-        }
+    onAddClick(record) {
+        console.log(record)
     }
+
+    onEditClick(record) {
+        console.log(record)
+    }
+
+    onDeleteClick(record) {
+        console.log(record)
+    }
+
+
 
     render() {
+        var pagination = {
+            current: 1,
+            pageSize: 10,
+        }
         return (
             <>
                 <Row>
@@ -53,13 +86,21 @@ export default class Home extends React.Component {
                         <Title>FNST</Title>
                     </Col>
                     <Col type="flex" justify="center" align="right"span={23}>
-                        <Button type="primary" onClick={this.onClick.bind(this)} >New</Button>
+                        <Button type="primary" onClick={this.onNewClick.bind(this)}>New</Button>
                     </Col>
                     <Col type="flex" justify="center" align="middle" span={24}>
-                        <Table dataSource={this.state.dataSource} onExpand={this.onExpand.bind(this)} >
-                            <Column title="Name" dataIndex="name" key="name" />
-                            <Column title="Type" dataIndex="type" key="type" />
-                            <Column title="action" dataIndex="action" key="action" />
+                        <Table dataSource={this.state.groupDataSource} expandedRowRender={this.expandedRowRender.bind(this)} pagination={pagination}>
+                            <Column title="Name" dataIndex="name" key="name"/>
+                            <Column title="Action" dataIndex="action" key="action" render={
+                                function (_, record) {
+                                    return (
+                                        <Space size="middle">
+                                            <Button type="primary" onClick={this.onAddClick.bind(this, record)}>Add</Button>
+                                            <Button type="primary" onClick={this.onDeleteClick.bind(this, record)}>Delete</Button>
+                                        </Space>
+                                    )
+                                }.bind(this)
+                            }/>
                         </Table>
                     </Col>
                 </Row>
