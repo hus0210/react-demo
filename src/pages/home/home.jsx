b5fa1efad3157f7d47907582f81d39f40ca6eeb4
zouyu <zouy.fnst@cn.fujitsu.com>
2020-07-16 08:58:10 +0800

    use axios instead of fetch, add http proxy avoid cros

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './home.css'
 import { Row, Col, Calendar, Badge, Modal } from 'antd';
+import Axios from 'axios';
 
 export default class Home extends React.Component {
     state = {
@@ -8,40 +9,16 @@ export default class Home extends React.Component {
     };
     firstSelect = false;
 
-    getListData(value) {
-        let listData;
-        switch (value.date()) {
-            case 8:
-                listData = [
-                    { type: 'warning', content: 'This is warning event.' },
-                    { type: 'success', content: 'This is usual event.' },
-                ];
-                break;
-            case 10:
-                listData = [
-                    { type: 'warning', content: 'This is warning event.' },
-                    { type: 'success', content: 'This is usual event.' },
-                    { type: 'error', content: 'This is error event.' },
-                ];
-                break;
-            case 15:
-                listData = [
-                    { type: 'warning', content: 'This is warning event' },
-                    { type: 'success', content: 'This is very long usual event。。....' },
-                    { type: 'error', content: 'This is error event 1.' },
-                    { type: 'error', content: 'This is error event 2.' },
-                    { type: 'error', content: 'This is error event 3.' },
-                    { type: 'error', content: 'This is error event 4.' },
-                ];
-                break;
-            default:
-        }
-        return listData || [];
+    async getUserData(date) {
+        var res = await Axios.post("/api/sign/showCourse/", {
+            eStart: date
+        })
+        console(res)
+        return []
     }
 
     dateCellRender(value) {
-        var data = this.getListData(value);
-
+        var data = [];
         if (data.length !== 0) {
             return (
                 <>
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './login.css'
 import LoginForm from 'components/loginForm/loginForm.jsx'
-import { Row, Col } from 'antd';
+import { message, Row, Col } from 'antd';
+import Axios from 'axios'
 
 export default class Login extends React.Component {
     onFinish(values) {
-        fetch(process.env.REACT_APP_BASE_URL + "/login", {
-            method: 'GET',
-            mode: 'cors',
+        Axios.post("/api/employee/login", {
+            eName: values.username,
+            ePwd: values.password,
         }).then(function (res) {
-            this.props.history.push('/home')
-            console.log(values, res)
+            console.log(res)
+            if (res.message === "登陆成功") {
+                message.success("登陆成功")
+                this.props.history.push('/home')
+            } else if (res.message === "账号不存在") {
+                message.error('账号不存在');
+            } else if (res.message === "秘密错误") {
+                message.error('秘密错误');
+            } else {
+                message.error('未知错误');
+            }
         }.bind(this))
-    }
-
-    onFinishFailed(errorInfo) {
-        console.log('Failed:', errorInfo);
     }
 
     render() {
@@ -24,7 +30,7 @@ export default class Login extends React.Component {
                 <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
                     <Col xs={2} sm={4} md={6} lg={8} xl={10} ></Col>
                     <Col xs={20} sm={16} md={12} lg={8} xl={4}>
-                        <LoginForm onFinish={this.onFinish.bind(this)} onFinishFailed={this.onFinishFailed.bind(this)} />
+                        <LoginForm onFinish={this.onFinish.bind(this)} />
                     </Col>
                     <Col xs={2} sm={4} md={6} lg={8} xl={10} ></Col>
                 </Row>
