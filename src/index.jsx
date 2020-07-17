829262f286ffa513892a43d8514053ace92abaa0
zouyu <zouy.fnst@cn.fujitsu.com>
2020-07-17 09:39:12 +0800

    use axios instead of fetch

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import Router from 'pages/route.jsx'
 
+Axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
 ReactDOM.render(
     <Router />, document.getElementById('root')
 );
